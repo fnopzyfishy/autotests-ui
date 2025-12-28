5325aea8a03844007bfe6a41118d713493d95093
fnopzyfishy <perec2742@gmail.com>
2025-12-28 16:56:57 +0500

    add loggs

--- components/base_component.py
+++ components/base_component.py
@@ -1,14 +1,27 @@
 from typing import Pattern
 
+import allure
+
 from playwright.sync_api import Page, expect
+from tools.logger import get_logger
+
+logger = get_logger("BASE_COMPONENT")
 
 
 class BaseComponent:
     def __init__(self, page: Page):
         self.page = page
-
+        
     def check_current_url(self, expected_url: Pattern[str]):
-        expect(self.page).to_have_url(expected_url)
+        step = 'Check current url'
+        
+        with allure.step(step):
+            logger.info(step)
+            expect(self.page).to_have_url(expected_url)
 
     def refresh_page(self):
-        self.page.reload()
+        step = 'Refresh page'
+        
+        with allure.step(step):
+            logger.info(step)
+            self.page.reload()
