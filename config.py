2fa354097517db31565e669103841ac2bd74951b
fnopzyfishy <perec2742@gmail.com>
2025-12-28 18:03:05 +0500

    add allure environment info

--- config.py
+++ config.py
@@ -28,7 +28,7 @@ class Settings(BaseSettings):
         env_file_encoding="utf-8",
         env_nested_delimiter=".",
     )
-
+    
     app_url: HttpUrl
     headless: bool
     browsers: list[Browser]
@@ -36,6 +36,7 @@ class Settings(BaseSettings):
     test_data: TestData
     videos_dir: DirectoryPath
     tracing_dir: DirectoryPath
+    allure_results_dir: DirectoryPath
     browser_state_file: FilePath
     
     def get_base_url(self) -> str:
@@ -45,16 +46,19 @@ class Settings(BaseSettings):
     def initialize(cls) -> Self:
         videos_dir = DirectoryPath("./videos")
         tracing_dir = DirectoryPath("./tracing")
+        allure_results_dir = DirectoryPath("./allure-results")
         browser_state_file = FilePath("browser-state.json")
         
         videos_dir.mkdir(exist_ok=True)
         tracing_dir.mkdir(exist_ok=True)
+        allure_results_dir.mkdir(exist_ok=True)
         browser_state_file.touch(exist_ok=True)
         
         return Settings(
             videos_dir=videos_dir,
             tracing_dir=tracing_dir,
-            browser_state_file=browser_state_file
+            allure_results_dir=allure_results_dir,
+            browser_state_file=browser_state_file,
         )
 
 
